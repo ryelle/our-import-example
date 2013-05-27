dd2e9889085277ff048f1877dcc6f51b901e2861
Kelly Dwan <kelly.dwan@gmail.com>
2013-05-26 23:17:25 -0400

    Add Hello command

--- our-cli.php
+++ our-cli.php
@@ -6,6 +6,17 @@
  */
 class Our_Import extends WP_CLI_Command {
 
+	/**
+	 * Create the hello subcommand.
+	 * @param $args        array  arguments by position, starting at 0
+	 * @param $assoc_args  array  arguments passed in as --key=value, associative
+	 * @synopsis <name>
+	 */
+	public function hello( $args = array(), $assoc_args = array() ) {
+		list( $name ) = $args;
+		WP_CLI::success( "Hello $name." );
+	}
+
 }
 
 // Here we define the command name we want to use.
