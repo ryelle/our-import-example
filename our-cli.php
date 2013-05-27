4d1f09bba323304677374e696727382a2732ab7c
Kelly Dwan <kelly.dwan@gmail.com>
2013-05-26 23:33:20 -0400

    Add the DB connection setup

--- our-cli.php
+++ our-cli.php
@@ -5,6 +5,7 @@
  * the public methods are the subcommands.
  */
 class Our_Import extends WP_CLI_Command {
+	private $db;
 
 	/**
 	 * Create the hello subcommand.
@@ -15,6 +16,31 @@ class Our_Import extends WP_CLI_Command {
 	public function hello( $args = array(), $assoc_args = array() ) {
 		list( $name ) = $args;
 		WP_CLI::success( "Hello $name." );
+	}
+	
+
+
+	/**
+	 * Set up the PDO object. Connection info is hardcoded for this example.
+	 */
+	private function setup() {
+		$database = array(
+			'host'     => '127.0.0.1',
+			'port'     => '3306',
+			'database' => 'example_custom',
+			'user'     => 'wp',
+			'pass'     => 'wp',
+		);
+		extract( $database, EXTR_SKIP );
+		try {
+			$db = new PDO( 'mysql:host=' . $host . ';port=' . $port . ';dbname=' . $database, $user, $pass, array( PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'' ) );
+			
+			return $db;
+			
+		} catch ( PDOException $e ) {
+			WP_CLI::error( 'Could not connect to database: '. $e->getMessage() );
+			die();
+		}
 	}
 
 }
