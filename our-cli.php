8d6b4ec327e789fdef6487ef240bd397436eb150
Kelly Dwan <kelly.dwan@gmail.com>
2013-05-26 23:09:25 -0400

    Documentation

--- our-cli.php
+++ our-cli.php
@@ -1,6 +1,12 @@
 <?php
 
+/**
+ * Our_Import extends the WP_CLI_Command class, 
+ * the public methods are the subcommands.
+ */
 class Our_Import extends WP_CLI_Command {
 
 }
+
+// Here we define the command name we want to use.
 WP_CLI::add_command( 'ourport', 'Our_Import' );
