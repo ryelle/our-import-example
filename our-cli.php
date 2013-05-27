de10963b674f7cff9e2885d6d7e99bbf64e44d67
Kelly Dwan <kelly.dwan@gmail.com>
2013-05-27 14:57:42 -0400

    Add update & import function

--- our-cli.php
+++ our-cli.php
@@ -25,7 +25,74 @@ class Our_Import extends WP_CLI_Command {
 		$this->setup();
 		WP_CLI::success( "Databse connected!" );
 	}
+	
+	/**
+	 * Create a function to get one post from the non-WP database
+	 */
+	function update( $args = array(), $assoc_args = array() ) {
+		$this->setup();
+		$id = absint( $args[0] );
+		
+		$sql = "SELECT DISTINCT 
+				p.PostID AS imported_id, 
+				p.MemberID AS post_author, 
+				p.Title AS post_title,
+				p.Slug AS post_name, 
+				p.PublishDate AS post_date,
+				p.Contents AS post_content,
+				c.ContinuedContents AS more_contents
+			FROM posts p
+			LEFT JOIN postcontents c ON c.postid = p.postid
+			WHERE p.PostID = $id
+			LIMIT 1";
+		$stmt = $this->db->prepare( $sql );
+		$stmt->execute();
+		$post = $stmt->fetch( PDO::FETCH_ASSOC );
+		
+		$this->_import( $post );
+	}
+	
+	
+	/**
+	 * Function to process a single row from the non-WP database, and insert into new DB
+	 */
+	private function _import( $post ) {
 
+		$content = $post['post_content'];
+		
+		// This particular DB stores a 2nd page of content in that other table
+		if ( ! empty( $post['more_content'] ) )
+			$content .= "\n\n". $post['more_content'];
+
+		// Here we'd process the content for any URL changes, or
+		// anything else that amounts to string replacement.
+		
+		// We would also do any author mapping here.
+		$author = $this->get_wordpress_user( $post['post_author'] );
+		
+		$new_post = array(
+			'post_author'       => $author,
+			'post_content'      => $content,
+			'post_date'         => $post['post_date'],
+			'post_name'         => $post['post_name'],
+			'post_title'        => $post['post_title'],
+			// We're setting this for all imported posts, 
+			'post_status'       => 'publish',
+			'post_type'         => 'post'
+		);
+		$wp_id = wp_insert_post( $new_post );
+		if ( $wp_id ) {
+			update_post_meta( $wp_id, '_imported_id', $post['imported_id'] );
+			
+			// Now that we have the WP ID, we can go through the content
+			// to grab any <img>s & upload them into the WP media library. 
+			// -- you need the WP ID to attach the image to this post.
+			
+			WP_CLI::success( "Successfully imported post $wp_id" );
+		} else {
+			WP_CLI::error( "Import of ".$post['imported_id']." failed." );
+		}
+	}
 
 	/**
 	 * Set up the PDO object. Connection info is hardcoded for this example.
@@ -36,7 +103,7 @@ class Our_Import extends WP_CLI_Command {
 			'port'     => '3306',
 			'name' => 'example_custom',
 			'user'     => 'meetup',
-			'pass'     => 'incorrect',
+			'pass'     => 'meetup',
 		);
 		extract( $database, EXTR_SKIP );
 		try {
@@ -47,6 +114,16 @@ class Our_Import extends WP_CLI_Command {
 			WP_CLI::error( 'Could not connect to database: '. $e->getMessage() );
 			die();
 		}
+	}
+	
+	/**
+	 * In a real import, this would be a real function.
+	 *  if your custom DB stores emails, you can use email_exists
+	 *  to check if that user has a WP account, and create using 
+	 *  wp_insert_user if not. 
+	 */
+	private function get_wordpress_user( $author ) {
+		return $author;
 	}
 
 }
